0b018db6cece11bd03677159664bd32e3b1a301e
antoinerobin <antoine.robin@vesperiagroup.com>
2014-11-24 17:06:51 +0100

    create updater

---
+++ scripts/update/class.Updater.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014 (original work) Open Assessment Technologies SA;
+ *
+ *
+ */
+
+/**
+ *
+ * @author Joel Bout <joel@taotesting.com>
+ */
+class taoMediaManager_scripts_update_Updater extends \common_ext_ExtensionUpdater {
+    
+    /**
+     * 
+     * @param string $currentVersion
+     * @return string $versionUpdatedTo
+     */
+    public function update($initialVersion) {
+        
+        $currentVersion = $initialVersion;
+
+        //migrate from 0.1 to 0.1.1
+        if ($currentVersion == '0.1') {
+
+            \oat\tao\model\media\MediaSource::addMediaSource('mediamanager', 'oat\taoMediaManager\model\MediaManagerBrowser', 'browser');
+            \oat\tao\model\media\MediaSource::addMediaSource('mediamanager', 'oat\taoMediaManager\model\MediaManagerManagement', 'management');
+        }
+        
+        return $currentVersion;
+    }
+}

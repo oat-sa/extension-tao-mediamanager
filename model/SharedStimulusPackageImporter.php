3e0403b1947f4c077abfa46b4bcce2a64f8745e8
antoinerobin <antoine.robin@vesperiagroup.com>
2015-05-18 09:56:13 +0200

    shared stimulus are now xhtml files

--- model/SharedStimulusPackageImporter.php
+++ model/SharedStimulusPackageImporter.php
@@ -113,7 +113,13 @@ class SharedStimulusPackageImporter extends ZipImporter
     {
         $service = MediaService::singleton();
         $filename = $this->validateXmlFile();
-        if (!$service->createMediaInstance($filename, $class->getUri(), $lang, basename($this->xmlFile))) {
+
+        $name = basename($this->xmlFile, 'xml');
+        $name .= 'xhtml';
+        $filepath = dirname($filename).$name;
+        \tao_helpers_File::copy($filename, $filepath);
+
+        if (!$service->createMediaInstance($filepath, $class->getUri(), $lang, $name)) {
             $report = \common_report_Report::createFailure(__('Fail to import Shared Stimulus'));
         } else {
             $report = \common_report_Report::createSuccess(__('Shared Stimulus imported successfully'));

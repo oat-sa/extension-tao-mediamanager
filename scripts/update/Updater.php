89691f5f834cf0fc22111d424a468abad7ef9036
Joel Bout <joel@taotesting.com>
2017-01-20 17:06:47 +0100

    Version bump 0.6.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -207,6 +207,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $currentVersion = '0.3.0';
         }
         
-        $this->skip('0.3.0','0.5.2');
+        $this->skip('0.3.0','0.6.0');
     }
 }

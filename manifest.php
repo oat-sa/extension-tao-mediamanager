ad41d21369bac3104c8c3da46cf09e2a66dc1b98
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-01-02 17:46:12 +0100

    bump version

--- manifest.php
+++ manifest.php
@@ -27,10 +27,10 @@ return array(
     'label' => 'extension-tao-mediamanager',
     'description' => 'TAO media manager extension',
     'license' => 'GPL-2.0',
-    'version' => '2.2.0',
+    'version' => '2.3.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
-        'tao' => '>=12.21.0',
+        'tao' => '>=15.0.0',
         'generis' => '>=5.9.0',
         'taoItems' => '>=2.9.0'
     ),

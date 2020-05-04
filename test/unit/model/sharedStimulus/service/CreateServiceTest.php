811cedca311b8e2a2a4c669b650bbff42b184cf8
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-05-04 17:00:40 +0200

    Create initial test

--- test/unit/model/sharedStimulus/service/CreateServiceTest.php
+++ test/unit/model/sharedStimulus/service/CreateServiceTest.php
@@ -20,12 +20,97 @@
 
 namespace oat\taoMediaManager\test\unit\model\sharedStimulus\service;
 
+use common_report_Report;
+use core_kernel_classes_Class;
+use oat\generis\model\data\Ontology;
 use oat\generis\test\TestCase;
+use oat\oatbox\session\SessionService;
+use oat\remoteProctoring\model\delivery\RemoteProctoringDeliverySettingsRepository;
+use oat\remoteProctoring\model\ProctorioApiService;
+use oat\tao\model\upload\UploadService;
+use oat\taoMediaManager\model\sharedStimulus\CreateCommand;
+use oat\taoMediaManager\model\sharedStimulus\service\CreateService;
+use oat\taoMediaManager\model\sharedStimulus\SharedStimulus;
+use oat\taoMediaManager\model\SharedStimulusImporter;
+use PHPUnit\Framework\MockObject\MockObject;
 
 class CreateServiceTest extends TestCase
 {
-    public function testCreate(): void
+    private const URI = 'uri';
+    private const CLASS_URI = 'uri';
+    private const LANGUAGE_URI = 'uri';
+    private const NAME = 'name';
+
+    /** @var CreateService */
+    private $service;
+
+    /** @var Ontology|MockObject */
+    private $ontology;
+
+    /** @var UploadService|MockObject */
+    private $uploadService;
+
+    /** @var SharedStimulusImporter|MockObject */
+    private $sharedStimulusImporter;
+
+    public function setUp(): void
     {
-        $this->markTestIncomplete('Waiting CR input to proceed'); //@TODO
+        $this->ontology = $this->createMock(Ontology::class);
+        $this->uploadService = $this->createMock(UploadService::class);
+        $this->sharedStimulusImporter = $this->createMock(SharedStimulusImporter::class);
+        $this->service = new CreateService();
+        $this->service->setServiceLocator(
+            $this->getServiceLocatorMock(
+                [
+                    Ontology::SERVICE_ID => $this->ontology,
+                    UploadService::SERVICE_ID => $this->uploadService,
+                    SharedStimulusImporter::class => $this->sharedStimulusImporter
+                ]
+            )
+        );
+
+        $kernelClass = $this->createMock(core_kernel_classes_Class::class);
+        $kernelClass->method('getInstances')
+            ->willReturn(1);
+
+        $this->ontology
+            ->method('getClass')
+            ->willReturn($kernelClass);
+
+        $childrenReport = $this->createMock(common_report_Report::class);
+        $childrenReport->method('getData')
+            ->willReturn(
+                [
+                    'uriResource' => self::URI
+                ]
+            );
+
+        $importReport = $this->createMock(common_report_Report::class);
+        $importReport->method('getChildren')
+            ->willReturn([$childrenReport]);
+
+        $this->sharedStimulusImporter
+            ->method('import')
+            ->willReturn($importReport);
+    }
+
+    public function testCreateSharedStimulus(): void
+    {
+        $command = new CreateCommand(
+            self::CLASS_URI,
+            self::NAME,
+            self::LANGUAGE_URI
+        );
+
+        $createdSharedStimulus = $this->service->create($command);
+
+        $this->assertEquals(
+            new SharedStimulus(
+                self::URI,
+                self::NAME,
+                self::LANGUAGE_URI
+            ),
+            $createdSharedStimulus
+        );
     }
 }

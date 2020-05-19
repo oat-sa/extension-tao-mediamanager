7c60b3c1f6f85da3c0b8d283f68860118296704a
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-05-19 14:09:15 +0200

    Adding tests skeleton

---
+++ test/unit/model/relation/event/ItemUpdatedProcessorTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoMediaManager\test\unit\model\relation\event;
+
+use oat\generis\test\TestCase;
+use oat\taoMediaManager\model\relation\event\MediaRelationListener;
+use oat\taoMediaManager\model\relation\event\processor\ItemUpdatedProcessor;
+use oat\taoMediaManager\model\relation\service\ItemRelationUpdateService;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class ItemUpdatedProcessorTest extends TestCase
+{
+    /** @var ItemUpdatedProcessor */
+    private $subject;
+
+    /** @var ItemRelationUpdateService|MockObject */
+    private $updateService;
+
+    public function setUp(): void
+    {
+        $this->updateService = $this->createMock(ItemRelationUpdateService::class);
+        $this->subject = new MediaRelationListener();
+        $this->subject->setServiceLocator(
+            $this->getServiceLocatorMock(
+                [
+                    ItemRelationUpdateService::class => $this->updateService,
+                ]
+            )
+        );
+    }
+
+    public function testProcess(): void
+    {
+        $this->markTestIncomplete('Wip');
+    }
+}

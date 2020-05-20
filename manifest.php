631c81a5cd02439cebc497fbf55e700c2d735a95
Camille Moyon <camille@taotesting.com>
2020-05-20 14:13:15 +0200

    Add dependencies to taoQtiItem

--- manifest.php
+++ manifest.php
@@ -32,7 +32,8 @@ return [
     'requires' => [
         'tao' => '>=42.5.0',
         'generis' => '>=12.17.0',
-        'taoItems' => '>=6.0.0'
+        'taoItems' => '>=6.0.0',
+        'taoQtiItem' => '>=24.2.0'
     ],
     'models' => [
         'http://www.tao.lu/Ontologies/TAOMedia.rdf'

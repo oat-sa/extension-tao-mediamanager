2d06cd5896b987cc2dca4d35705587c1b52697e6
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2020-05-25 13:30:34 +0200

    Remove from comments fake data

--- views/js/qtiCreator/helper/passageLoader.js
+++ views/js/qtiCreator/helper/passageLoader.js
@@ -70,29 +70,6 @@ define([
                         'zh-TW': 'Traditional Chinese from Taiwan'
                     }
 
-                    // var mockBody = {
-                    //     "serial": "container_containerstatic_5ec29c5c7806a515391441",
-                    //     "body": "\n    <p>SharedStimulus definition file is on XML format and so do not support \"&amp;\" symbol in remote image url, it requires to be escaped for <i>&amp;amp<\/i><\/p>\n    <p>{{img_5ec29c5c8b0d5286470339}}<\/p>\n    <p>{{img_5ec29c5c92311756247821}}<\/p>\n",
-                    //     "elements": {
-                    //         "img_5ec29c5c8b0d5286470339": {
-                    //             "serial": "img_5ec29c5c8b0d5286470339",
-                    //             "qtiClass": "img",
-                    //             "attributes": {
-                    //                 "src": "https:\/\/via.placeholder.com\/300x300.png?text=remote+shared+stimulus+media",
-                    //                 "alt": "my first image"
-                    //             },
-                    //         },
-                    //         "img_5ec29c5c92311756247821": {
-                    //             "serial": "img_5ec29c5c92311756247821",
-                    //             "qtiClass": "img",
-                    //             "attributes": {
-                    //                 "src": "https:\/\/via.placeholder.com\/300x300.png?text=another+remote+media",
-                    //                 "alt": "my first image"
-                    //             },
-                    //         }
-                    //     },
-                    // };
-
                     newItem = new Item().id(_generateIdentifier(config.uri)).attr('title', response.data.name);
                     newItem.data('new', true);
                     newItem.data('dummy', true);

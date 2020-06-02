c9c4cbdf45421003f247d8d869cb0a3e7f9f3b00
Camille Moyon <camille@taotesting.com>
2020-06-02 15:19:01 +0200

    Add extractor service

---
+++ model/relation/event/MediaSavedEventDispatcher.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoMediaManager\model\relation\event;
+
+use oat\oatbox\event\EventManager;
+use oat\oatbox\filesystem\File;
+use oat\oatbox\service\ConfigurableService;
+use oat\taoMediaManager\model\sharedStimulus\parser\SharedStimulusMediaExtractor;
+use oat\taoMediaManager\model\sharedStimulus\parser\SharedStimulusMediaParser;
+use tao_helpers_File;
+
+class MediaSavedEventDispatcher extends ConfigurableService
+{
+    public function dispatchFromContent(string $id, string $mimeType, string $content): void
+    {
+        $elementIds = $this->isSharedStimulus($mimeType)
+            ? $this->getSharedStimulusExtractor()->extractMediaIdentifiers($content)
+            : [];
+
+        $this->getEventManager()->trigger(new MediaSavedEvent($id, $elementIds));
+    }
+
+    /**
+     * @param string|File $fileSource
+     */
+    public function dispatchFromFile(string $id, $fileSource): void
+    {
+        $mimeType = $fileSource instanceof File ? $fileSource->getMimeType() : tao_helpers_File::getMimeType($fileSource);
+
+        if ($this->isSharedStimulus($mimeType)) {
+            $content = $fileSource instanceof File ? $fileSource->read() : file_get_contents($fileSource);
+        } else {
+            $content = '';
+        }
+
+        $this->dispatchFromContent($id, $mimeType, $content);
+    }
+
+    private function isSharedStimulus($mimeType): bool
+    {
+        return $mimeType === 'application/qti+xml';
+    }
+
+    private function getEventManager(): EventManager
+    {
+        return $this->getServiceLocator()->get(EventManager::SERVICE_ID);
+    }
+
+    private function getSharedStimulusExtractor(): SharedStimulusMediaExtractor
+    {
+        return $this->getServiceLocator()->get(SharedStimulusMediaExtractor::class);
+    }
+}

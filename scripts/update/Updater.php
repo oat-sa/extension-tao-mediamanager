9b72f415472e7b5d20f40854bf5c85d1ed93953e
gitromba <mihaly.hajdu@taotesting.com>
2020-06-10 10:44:10 +0200

    chore: bump manifest version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -35,6 +35,6 @@ class Updater extends \common_ext_ExtensionUpdater
             throw new \common_exception_NotImplemented('Updates from versions prior to Tao 3.1 are not longer supported, please update to Tao 3.1 first');
         }
 
-        $this->skip('0.3.0', '9.3.0');
+        $this->skip('0.3.0', '9.4.0');
     }
 }

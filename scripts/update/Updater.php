0bd539e73fa0f44021a2a21aa968c78a1319cd1e
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2020-06-18 17:29:53 +0200

    Bump PHP version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -101,6 +101,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('11.0.0');
         }
 
-        $this->skip('11.0.0', '11.0.1');
+        $this->skip('11.0.0', '11.0.2');
     }
 }

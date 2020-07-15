6b98e6b55d126c00f1470cc4c4fb4b1b6918fe00
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-07-15 11:40:53 +0200

    proposal

---
+++ model/relation/task/AbstractRelationshipTask2.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoMediaManager\model\relation\task;
+
+use common_exception_MissingParameter;
+use common_report_Report;
+use Iterator;
+use oat\generis\model\OntologyAwareTrait;
+use oat\oatbox\action\Action;
+use oat\tao\model\taskQueue\QueueDispatcherInterface;
+use oat\tao\model\taskQueue\Task\CallbackTaskInterface;
+use oat\tao\model\taskQueue\Task\TaskAwareInterface;
+use oat\tao\model\taskQueue\Task\TaskAwareTrait;
+use Throwable;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
+
+abstract class AbstractRelationshipTask2 implements Action, ServiceLocatorAwareInterface, TaskAwareInterface
+{
+    use ServiceLocatorAwareTrait;
+    use OntologyAwareTrait;
+    use TaskAwareTrait;
+    use PositionTrackTrait;
+
+    public const CACHE_KEY = '::_last_known';
+
+    /** @var int */
+    private $affected;
+
+    /** @var int */
+    private $pickSize;
+
+    /** @var common_report_Report */
+    private $anomalies;
+
+    public function __invoke($params)
+    {
+        if (count($params) < 4) {
+            throw new common_exception_MissingParameter();
+        }
+        $start = array_shift($params);
+        $chunkSize = array_shift($params);
+        $this->pickSize = $pickSize = array_shift($params);
+        $repeat = (bool)array_shift($params);
+
+        $max = $this->getLastRowNumber();
+
+        $this->keepCurrentPosition($start);
+
+        $end = $start + $chunkSize;
+
+        if ($end >= $max) {
+            $end = $max;
+        }
+
+        $itemClasses = $this->getTargetClasses();
+
+        $iterator = $this->getServiceLocator()
+            ->get(TaskIterator::class)
+            ->getIterator($itemClasses, $start, $end);
+
+        $this->initAnomaliesCollector();
+
+        iterator_apply(
+            $iterator,
+            [$this, 'applyProcessor'],
+            [$iterator, $pickSize, $this->affected]
+        );
+
+        if ($repeat) {
+            $nStart = $end + 1;
+            if ($nStart + $chunkSize <= $max) {
+                $this->selfRepeat($nStart, $chunkSize, $pickSize, $repeat);
+            }
+        }
+
+        $report = common_report_Report::createSuccess(
+            sprintf("Items in range from %s to %s proceeded in amount of %s", $start, $end, $this->affected)
+        );
+
+        if ($this->anomalies->hasChildren()) {
+            $report->add($this->anomalies);
+        }
+
+        return $report;
+    }
+
+    abstract protected function getTargetClasses(): array;
+
+    abstract protected function processUnit(array $unit): void;
+
+    private function selfRepeat(int $start, int $chunkSize, int $pickSize, bool $repeat): CallbackTaskInterface
+    {
+        /** @var QueueDispatcherInterface $queueDispatcher */
+        $queueDispatcher = $this->getServiceLocator()->get(QueueDispatcherInterface::SERVICE_ID);
+
+        return $queueDispatcher->createTask(
+            new static(),
+            [$start, $chunkSize, $pickSize, $repeat],
+            sprintf(
+                'Relation recovery for %s started from %s with chunk size of %s',
+                self::class,
+                $start,
+                $chunkSize
+            )
+        );
+    }
+
+    private function applyProcessor(Iterator $iterator): bool
+    {
+        $unit = $iterator->current();
+
+        ++$this->affected;
+
+        try {
+            $this->processUnit($unit);
+        } catch (Throwable $exception) {
+            $this->addAnomaly($unit['id'], $unit['subject'], $exception->getMessage());
+        }
+
+        return $this->pickSize ? $this->affected < $this->pickSize * 2 : true;
+    }
+
+    private function initAnomaliesCollector(): void
+    {
+        $this->anomalies = common_report_Report::createInfo('Anomalies list');
+    }
+
+    private function addAnomaly(string $id, string $uri, string $reason): void
+    {
+        $this->anomalies->add(new common_report_Report(common_report_Report::TYPE_WARNING, $reason, [$id, $uri]));
+    }
+
+    private function getLastRowNumber(): int
+    {
+        return $this->getServiceLocator()->get(StatementLastIdRetriever::class)->retrieve();
+    }
+}
---
+++ model/relation/task/ItemToMediaRelationshipTask2.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoMediaManager\model\relation\task;
+
+use oat\tao\model\TaoOntology;
+use oat\taoQtiItem\model\qti\event\UpdatedItemEventDispatcher;
+use oat\taoQtiItem\model\qti\Service;
+
+class ItemToMediaRelationshipTask2 extends AbstractRelationshipTask2
+{
+    protected function getTargetClasses(): array
+    {
+        return $this->getClass(TaoOntology::CLASS_URI_ITEM)->getSubClasses(true);
+    }
+
+    protected function processUnit(array $unit): void
+    {
+        $resource = $this->getResource($unit['subject']);
+
+        $item = $this->getQtiService()->getDataItemByRdfItem($resource);
+
+        if ($item) {
+            $this->getUpdatedItemEventDispatcher()->dispatch($item, $resource);
+        }
+    }
+
+    private function getUpdatedItemEventDispatcher(): UpdatedItemEventDispatcher
+    {
+        return $this->getServiceLocator()->get(UpdatedItemEventDispatcher::class);
+    }
+
+    private function getQtiService(): Service
+    {
+        return $this->getServiceLocator()->get(Service::class);
+    }
+}

fd031a6d75a45f85cc6b036a4a15e88d310d5374
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-07-17 16:56:51 +0200

    add missing unit tests

---
+++ test/unit/model/relation/task/ItemToMediaRelationshipMigrationTaskTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoMediaManager\test\unit\model\relation\task;
+
+use oat\generis\test\TestCase;
+use oat\taoMediaManager\model\relation\task\ItemToMediaRelationshipMigrationTask;
+use oat\taoMediaManager\model\relation\task\ItemToMediaUnitProcessor;
+use ReflectionMethod;
+
+class ItemToMediaRelationshipMigrationTaskTest extends TestCase
+{
+    /** @var ItemToMediaUnitProcessor */
+    private $processor;
+
+    /** @var ItemToMediaRelationshipMigrationTask */
+    private $subject;
+
+    public function setUp(): void
+    {
+        $this->processor = $this->createMock(ItemToMediaUnitProcessor::class);
+        $this->subject = $this->getMockForAbstractClass(ItemToMediaRelationshipMigrationTask::class);
+        $this->subject->setServiceLocator(
+            $this->getServiceLocatorMock(
+                [
+                    ItemToMediaUnitProcessor::class => $this->processor,
+                ]
+            )
+        );
+    }
+
+    public function testGetUnitProcessor(): void
+    {
+        $reflectionMethod = new ReflectionMethod($this->subject, 'getUnitProcessor');
+        $reflectionMethod->setAccessible(true);
+
+        $this->assertSame($this->processor, $reflectionMethod->invoke($this->subject));
+    }
+}
---
+++ test/unit/model/relation/task/MediaToMediaRelationMigrationTaskTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoMediaManager\test\unit\model\relation\task;
+
+use oat\generis\test\TestCase;
+use oat\taoMediaManager\model\relation\task\ItemToMediaRelationshipMigrationTask;
+use oat\taoMediaManager\model\relation\task\MediaToMediaRelationMigrationTask;
+use oat\taoMediaManager\model\relation\task\MediaToMediaUnitProcessor;
+use ReflectionMethod;
+
+class MediaToMediaRelationMigrationTaskTest extends TestCase
+{
+    /** @var MediaToMediaUnitProcessor */
+    private $processor;
+
+    /** @var MediaToMediaRelationMigrationTask */
+    private $subject;
+
+    public function setUp(): void
+    {
+        $this->processor = $this->createMock(MediaToMediaUnitProcessor::class);
+        $this->subject = $this->getMockForAbstractClass(MediaToMediaRelationMigrationTask::class);
+        $this->subject->setServiceLocator(
+            $this->getServiceLocatorMock(
+                [
+                    MediaToMediaUnitProcessor::class => $this->processor,
+                ]
+            )
+        );
+    }
+
+    public function testGetUnitProcessor(): void
+    {
+        $reflectionMethod = new ReflectionMethod($this->subject, 'getUnitProcessor');
+        $reflectionMethod->setAccessible(true);
+
+        $this->assertSame($this->processor, $reflectionMethod->invoke($this->subject));
+    }
+}

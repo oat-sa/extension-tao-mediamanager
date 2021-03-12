353d47857205b76fae1d67bc9b3e8a91d6b09bdf
Nikita Pimenov <nikita.pimenov@taotesting.com>
2021-03-12 10:57:12 +0300

    chore: fix copyright

--- model/sharedStimulus/css/factory/CommandFactory.php
+++ model/sharedStimulus/css/factory/CommandFactory.php
@@ -15,19 +15,19 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
  */
 
 declare(strict_types=1);
 
 namespace oat\taoMediaManager\model\sharedStimulus\css\factory;
 
-use common_exception_InvalidArgumentType;
-use common_exception_MissingParameter;
 use oat\oatbox\service\ConfigurableService;
 use oat\taoMediaManager\model\sharedStimulus\css\LoadCommand;
 use oat\taoMediaManager\model\sharedStimulus\css\SaveCommand;
 use Psr\Http\Message\ServerRequestInterface;
+use common_exception_InvalidArgumentType as InvalidParameterException;
+use common_exception_MissingParameter as MissingParameterException;
 
 class CommandFactory extends ConfigurableService
 {
@@ -36,20 +36,20 @@ class CommandFactory extends ConfigurableService
         $parsedBody = $request->getParsedBody();
 
         if (!isset($parsedBody['uri'])) {
-            throw new common_exception_MissingParameter('uri', __METHOD__);
+            throw new MissingParameterException('uri', __METHOD__);
         }
 
         if (!isset($parsedBody['stylesheetUri'])) {
-            throw new common_exception_MissingParameter('stylesheetUri', __METHOD__);
+            throw new MissingParameterException('stylesheetUri', __METHOD__);
         }
 
         if (!isset($parsedBody['cssJson'])) {
-            throw new common_exception_MissingParameter('cssJson', __METHOD__);
+            throw new MissingParameterException('cssJson', __METHOD__);
         }
 
         $css = json_decode($parsedBody['cssJson'], true);
         if (!is_array($css)) {
-            throw new common_exception_InvalidArgumentType(
+            throw new InvalidParameterException(
                 __CLASS__,
                 \Context::getInstance()->getActionName(),
                 3,
@@ -70,11 +70,11 @@ class CommandFactory extends ConfigurableService
 
 
         if (!isset($parsedBody['uri'])) {
-            throw new common_exception_MissingParameter('uri', __METHOD__);
+            throw new MissingParameterException('uri', __METHOD__);
         }
 
         if (!isset($parsedBody['stylesheetUri'])) {
-            throw new common_exception_MissingParameter('stylesheetUri', __METHOD__);
+            throw new MissingParameterException('stylesheetUri', __METHOD__);
         }
 
         return new LoadCommand(

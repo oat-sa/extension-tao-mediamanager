12585b60a8e6a0e2e1b2ebf87258c3b34649d777
Chinnu Francis <chinnu@focaloid.com>
2021-07-23 18:25:34 +0530

    chore: add setRolesPermissions file

---
+++ scripts/install/SetRolesPermissions.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoMediaManager\scripts\install;
+
+use oat\taoMediaManager\controller\MediaManager;
+use oat\oatbox\extension\InstallAction;
+use oat\taoMediaManager\model\classes\user\TaoAssetRoles;
+use oat\tao\model\accessControl\ActionAccessControl;
+use oat\tao\scripts\tools\accessControl\SetRolesAccess;
+
+class SetRolesPermissions extends InstallAction
+{
+    private const CONFIG = [
+        SetRolesAccess::CONFIG_PERMISSIONS => [
+            MediaManager::class => [
+                'editClassLabel' => [
+                    TaoAssetRoles::ASSET_CLASS_NAVIGATOR => ActionAccessControl::READ
+                ],
+                'editInstance' => [
+                    TaoAssetRoles::ASSET_VIEWER => ActionAccessControl::READ,
+                    TaoAssetRoles::ASSET_PREVIEWER => ActionAccessControl::READ,
+                    TaoAssetRoles::ASSET_PROPERTIES_EDITOR => ActionAccessControl::WRITE,
+                    TaoAssetRoles::ASSET_CONTENT_CREATOR => ActionAccessControl::WRITE,
+                ],
+            ],
+        ],
+    ];
+
+    public function __invoke($params = [])
+    {
+        $setRolesAccess = $this->propagate(new SetRolesAccess());
+        $setRolesAccess([
+            '--' . SetRolesAccess::OPTION_CONFIG, self::CONFIG,
+        ]);
+    }
+}

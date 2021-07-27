a8a1c161a681f99570d9bb52b5e5243708af7619
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2021-07-27 16:17:34 +0200

    chore: Small fix on xpath button

--- views/cypress/utils/selectors.js
+++ views/cypress/utils/selectors.js
@@ -4,6 +4,6 @@ export default {
     moveConfirmSelector: 'button[data-control="ok"]',
     assetForm: 'form[action="/taoMediaManager/MediaManager/editInstance"]',
     assetClassForm: 'form[action="/taoMediaManager/MediaManager/editClassLabel"]',
-    deleteConfirm: '[data-control="delete"]',
+    deleteConfirm: 'button[data-control="ok"]',
     root: '[data-uri="http://www.tao.lu/Ontologies/TAOMedia.rdf#Media"]'
 };

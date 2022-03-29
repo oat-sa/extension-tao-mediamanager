9987a883c958238efdcfa8e34c6f2a26a19593c7
Andrei Shapiro <shpr.andrew@gmail.com>
2022-03-29 17:29:04 +0000

    refactor: change to script action, check for file availability, wrap into try-catch

--- scripts/ReplaceSharedStimulusMedia.php
+++ scripts/ReplaceSharedStimulusMedia.php
@@ -15,106 +15,160 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2021-2022 (original work) Open Assessment Technologies SA.
  */
 
 declare(strict_types=1);
 
 namespace oat\taoMediaManager\scripts;
 
-use core_kernel_classes_Literal;
-use oat\generis\model\OntologyAwareTrait;
-use oat\oatbox\action\Action;
+use Throwable;
 use oat\oatbox\filesystem\File;
-use oat\oatbox\filesystem\FileSystemService;
+use core_kernel_classes_Literal;
 use oat\oatbox\reporting\Report;
-use oat\oatbox\service\ConfigurableService;
-use oat\taoMediaManager\model\fileManagement\FileSourceUnserializer;
-use oat\taoMediaManager\model\fileManagement\FlySystemManagement;
+use core_kernel_classes_Property;
+use core_kernel_classes_Resource;
+use oat\generis\model\data\Ontology;
 use oat\taoMediaManager\model\MediaService;
-use core_kernel_classes_Resource as RdfResource;
+use oat\oatbox\filesystem\FileSystemService;
+use oat\oatbox\extension\script\ScriptAction;
+use oat\taoMediaManager\model\fileManagement\FlySystemManagement;
 use oat\taoMediaManager\model\sharedStimulus\service\StoreService;
+use oat\taoMediaManager\model\fileManagement\FileSourceUnserializer;
 use oat\taoMediaManager\model\sharedStimulus\specification\SharedStimulusResourceSpecification;
 
 /**
- * Used to update old media from the command line
- *
- * ```
- * sudo -u www-data php index.php 'oat\taoMediaManager\scripts\ReplaceSharedStimulusMedia' [dryRun]
- * ```
+ * sudo -u www-data php index.php 'oat\taoMediaManager\scripts\ReplaceSharedStimulusMedia' [--dryRun]
  */
-class ReplaceSharedStimulusMedia extends ConfigurableService implements Action
+class ReplaceSharedStimulusMedia extends ScriptAction
 {
-    use OntologyAwareTrait;
+    public const OPTION_DRY_RUN = 'dryRun';
 
-    /**
-     * @param $params
-     * @return Report
-     */
-    public function __invoke($params)
+    /** @var core_kernel_classes_Property */
+    private $propertyLink;
+
+    /** @var string */
+    private $fileSystemId;
+
+    protected function provideOptions(): array
     {
-        $mediaInstances = $this->getMediaService()->getRootClass()->getInstances(true);
+        return [
+            self::OPTION_DRY_RUN => [
+                'prefix' => 'd',
+                'longPrefix' => self::OPTION_DRY_RUN,
+                'flag' => true,
+                'defaultValue' => false,
+                'description' => 'Get total shared stimulus media and number of shared stimulus media to modify',
+            ],
+        ];
+    }
 
-        $report = Report::createInfo(__('%s media on this environment', count($mediaInstances)));
+    protected function provideDescription(): string
+    {
+        return 'Move shared stimulus stored as single files to sub-folders';
+    }
+
+    protected function run(): Report
+    {
         $sharedStimulusCount = 0;
         $toBeChangedCount = 0;
         $successCount = 0;
+        $notExistsCount = 0;
+        $errorsCount = 0;
 
-        $dryRun = isset($params[0]) && $params[0] === 'dryRun';
+        $dryRun = $this->hasOption(self::OPTION_DRY_RUN);
+
+        $mediaInstances = $this->getMediaService()->getRootClass()->getInstances(true);
+        $report = Report::createInfo(__('%s media on this environment', count($mediaInstances)));
 
         foreach ($mediaInstances as $mediaInstance) {
-            if (!$this->getSharedStimulusResourceSpecification()->isSatisfiedBy($mediaInstance)) {
-                continue;
-            }
-
-            $sharedStimulusCount++;
-
-            $link = $this->getLinkPropertyValue($mediaInstance);
-            if (!$link) {
-                $report->add(Report::createWarning('Link property value is not literal'));
-                continue;
-            }
-
-            $report->add(Report::createInfo(__('Current path: %s', $link)));
-            $sharedStimulusStoredSourceFile = $this->getFile($link);
-
-            if (dirname($link) === '.') {
-                $toBeChangedCount++;
-
-                if (!$dryRun) {
-                    $dirname = $this->getSharedStimulusStoreService()->store($sharedStimulusStoredSourceFile, $link, []
-                    );
-                    $newMediaLink = $dirname . DIRECTORY_SEPARATOR . $link;
-                    $report->add(Report::createInfo(__('New path %s', $newMediaLink)));
-                    if ($mediaInstance->editPropertyValues(
-                        $this->getProperty(MediaService::PROPERTY_LINK),
-                        $newMediaLink
-                    )) {
-                        $sharedStimulusStoredSourceFile->delete();
-                        $successCount++;
-                    } else {
-                        $report->add(Report::createError(__('Issue while modifying %s', $mediaInstance->getUri())));
-                    }
+            try {
+                if (!$this->getSharedStimulusResourceSpecification()->isSatisfiedBy($mediaInstance)) {
+                    continue;
                 }
+
+                ++$sharedStimulusCount;
+
+                $link = $this->getLinkPropertyValue($mediaInstance);
+
+                if (!$link) {
+                    $report->add(Report::createWarning('Link property value is not literal'));
+
+                    continue;
+                }
+
+                $report->add(Report::createInfo(__('Current path: %s', $link)));
+
+                if (dirname($link) !== '.') {
+                    continue;
+                }
+
+                ++$toBeChangedCount;
+
+                if ($dryRun) {
+                    continue;
+                }
+
+                $sharedStimulusStoredSourceFile = $this->getFile($link);
+
+                if (!$sharedStimulusStoredSourceFile->exists()) {
+                    ++$notExistsCount;
+                    $report->add(
+                        Report::createWarning(
+                            __(
+                                'Shared stimulus "%s": file "%s" not exists',
+                                $mediaInstance->getUri(),
+                                $sharedStimulusStoredSourceFile->getBasename()
+                            )
+                        )
+                    );
+
+                    continue;
+                }
+
+                $dirname = $this->getStoreService()->store($sharedStimulusStoredSourceFile, $link, []);
+                $newMediaLink = $dirname . DIRECTORY_SEPARATOR . $link;
+                $report->add(Report::createInfo(__('New path %s', $newMediaLink)));
+
+                if (!$mediaInstance->editPropertyValues($this->getPropertyLink(), $newMediaLink)) {
+                    $report->add(Report::createError(__('Issue while modifying %s', $mediaInstance->getUri())));
+
+                    continue;
+                }
+
+                $sharedStimulusStoredSourceFile->delete();
+                ++$successCount;
+            } catch (Throwable $exception) {
+                ++$errorsCount;
+                $report->add(Report::createError(__('Issue while processing "%s"', $mediaInstance->getUri())));
+                $this->logError(
+                    sprintf(
+                        'Issue while processing "%s": %s',
+                        $mediaInstance->getUri(),
+                        $exception->getMessage()
+                    )
+                );
             }
         }
 
-        $report->add(Report::createSuccess(__('%s shared stimulus media to modify', $toBeChangedCount)));
         $report->add(Report::createInfo(__('Total shared stimulus media: %s', $sharedStimulusCount)));
-
+        $report->add(Report::createInfo(__('%s shared stimulus media to modify', $toBeChangedCount)));
 
         if (!$dryRun) {
+            $report->add(Report::createError(__('%s errors while processing shared stimulus media', $errorsCount)));
+            $report->add(Report::createWarning(__('%d shared stimulus media not exists', $notExistsCount)));
             $report->add(Report::createSuccess(__('%s media successfully modified', $successCount)));
         }
 
         return $report;
     }
 
-    private function getLinkPropertyValue(RdfResource $mediaInstance): ?string
+    private function getLinkPropertyValue(core_kernel_classes_Resource $mediaInstance): ?string
     {
-        $propertyValue = $mediaInstance->getUniquePropertyValue($this->getProperty(MediaService::PROPERTY_LINK));
+        $propertyValue = $mediaInstance->getUniquePropertyValue($this->getPropertyLink());
+
         if ($propertyValue instanceof core_kernel_classes_Literal) {
-            return $this->getFileSourceUnserializer()->unserialize((string)$propertyValue);
+            return $this->getFileSourceUnserializer()->unserialize((string) $propertyValue);
         }
 
         return null;
@@ -123,38 +177,57 @@ class ReplaceSharedStimulusMedia extends ConfigurableService implements Action
     private function getFile($link): File
     {
         return $this->getFileSystemService()
-            ->getDirectory($this->getFlySystemManagement()->getOption(FlySystemManagement::OPTION_FS))
+            ->getDirectory($this->getFileSystemId())
             ->getFile($link);
+    }
+
+    private function getPropertyLink(): core_kernel_classes_Property
+    {
+        if (!isset($this->propertyLink)) {
+            $this->propertyLink = $this->getOntology()->getProperty(MediaService::PROPERTY_LINK);
+        }
+
+        return $this->propertyLink;
+    }
+
+    private function getOntology(): Ontology
+    {
+        return $this->getServiceManager()->get(Ontology::SERVICE_ID);
     }
 
     private function getMediaService(): MediaService
     {
-        return $this->getServiceLocator()->get(MediaService::class);
+        return $this->getServiceManager()->get(MediaService::class);
     }
 
     private function getSharedStimulusResourceSpecification(): SharedStimulusResourceSpecification
     {
-        return $this->getServiceLocator()->get(SharedStimulusResourceSpecification::class);
+        return $this->getServiceManager()->get(SharedStimulusResourceSpecification::class);
     }
 
-    private function getSharedStimulusStoreService(): StoreService
+    private function getStoreService(): StoreService
     {
-        return $this->getServiceLocator()->get(StoreService::class);
+        return $this->getServiceManager()->get(StoreService::class);
     }
 
     private function getFileSourceUnserializer(): FileSourceUnserializer
     {
-        return $this->getServiceLocator()->get(FileSourceUnserializer::class);
+        return $this->getServiceManager()->get(FileSourceUnserializer::class);
     }
 
-    private function getFlySystemManagement(): FlySystemManagement
+    private function getFileSystemId(): string
     {
-        return $this->getServiceLocator()->get(FlySystemManagement::SERVICE_ID);
+        if (!isset($this->fileSystemId)) {
+            $flySystemManagement = $this->getServiceManager()->get(FlySystemManagement::SERVICE_ID);
+
+            $this->fileSystemId = $flySystemManagement->getOption(FlySystemManagement::OPTION_FS);
+        }
+
+        return $this->fileSystemId;
     }
 
     private function getFileSystemService(): FileSystemService
     {
-        return $this->getServiceLocator()->get(FileSystemService::SERVICE_ID);
+        return $this->getServiceManager()->get(FileSystemService::SERVICE_ID);
     }
 }
-

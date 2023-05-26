36e385ced235e747b284d234957b568a2d87c66b
Héctor Arroyo <hector.arroyo@taotesting.com>
2023-05-26 17:21:33 +0200

    feat: Add PHP-MD step in the pipelines

---
+++ controller/HighCC.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace oat\taoMediaManager\controller;
+
+// Cyclomatic Complexity = 11
+class HighCC
+{
+    public function example()
+    {
+        if ($a == $b) {
+            if ($a1 == $b1) {
+                fiddle();
+            } elseif ($a2 == $b2) {
+                fiddle();
+            } else {
+                fiddle();
+            }
+        } elseif ($c == $d) {
+            while ($c == $d) {
+                fiddle();
+            }
+        } elseif ($e == $f) {
+            for ($n = 0; $n < $h; $n++) {
+                fiddle();
+            }
+        } else {
+            switch ($z) {
+                case 1:
+                    fiddle();
+                    break;
+                case 2:
+                    fiddle();
+                    break;
+                case 3:
+                    fiddle();
+                    break;
+                default:
+                    fiddle();
+                    break;
+            }
+        }
+    }
+}

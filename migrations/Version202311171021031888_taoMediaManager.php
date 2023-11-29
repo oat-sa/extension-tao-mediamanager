e3f8f53dfa737df8ea2104ae7cebdb551367f0fc
Makar <sichevoy@gmail.com>
2023-11-29 15:52:46 +0200

    fix: Neo4j - impossible to save or author passage due to language property erased when "Save" button is clicked in "Edit Passage Properties" (#494)
    * fix: Neo4j - impossible to save or author passage due to language property erased when "Save" button is clicked in "Edit Passage Properties"
    
    ---------
    
    Co-authored-by: Makar Sichevoi <makar.sichevoy@taotesting.com>

---
+++ migrations/Version202311171021031888_taoMediaManager.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\taoMediaManager\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\Exception\IrreversibleMigration;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\tao\scripts\update\OntologyUpdater;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ *
+ * phpcs:disable Squiz.Classes.ValidClassName
+ */
+final class Version202311171021031888_taoMediaManager extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Changed http://www.tao.lu/Ontologies/TAOMedia.rdf#Language is_language_dependent to False';
+    }
+
+    public function up(Schema $schema): void
+    {
+        OntologyUpdater::syncModels();
+    }
+
+    public function down(Schema $schema): void
+    {
+        throw new IrreversibleMigration('Reverting this migration does not make sense');
+    }
+}

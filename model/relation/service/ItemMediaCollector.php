5dcb4befda514d3cb2ee9f9e03c5449f32030e32
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2025-02-17 15:41:22 +0100

    feat: item duplication is now update shared stimulus relations

--- model/relation/service/ItemMediaCollector.php
+++ model/relation/service/ItemMediaCollector.php
@@ -25,6 +25,7 @@ namespace oat\taoMediaManager\model\relation\service;
 use common_Exception;
 use core_kernel_classes_Resource as Resource;
 use oat\generis\model\data\Ontology;
+use oat\taoQtiItem\model\qti\container\ContainerItemBody;
 use oat\taoQtiItem\model\qti\Element;
 use oat\taoQtiItem\model\qti\Service as ItemsService;
 use tao_helpers_Uri;
@@ -42,8 +43,14 @@ class ItemMediaCollector
     {
         $mediaUris = [];
         $itemResource = $this->ontology->getResource($itemUri);
-        foreach ($this->getImgElements($itemResource) as $element) {
+        $itemBody = $this->itemsService->getDataItemByRdfItem($itemResource)->getBody();
+
+        foreach ($this->getImgElements($itemBody) as $element) {
             $mediaUris[] = tao_helpers_Uri::decode(str_replace('taomedia://mediamanager/', '', $element->getAttributeValue('src')));
+        }
+
+        foreach ($this->getSharedStimulus($itemBody) as $element) {
+            $mediaUris[] = tao_helpers_Uri::decode(str_replace('taomedia://mediamanager/', '', $element->getAttributeValue('href')));
         }
 
         return $mediaUris;
@@ -53,11 +60,13 @@ class ItemMediaCollector
      * @return Element[]
      * @throws common_Exception
      */
-    private function getImgElements(Resource $itemResource): array
+    private function getImgElements($itemBody): array
     {
-        return $this->itemsService
-            ->getDataItemByRdfItem($itemResource)
-            ->getBody()
-            ->getComposingElements('oat\taoQtiItem\model\qti\Img');
+        return $itemBody->getComposingElements('oat\taoQtiItem\model\qti\Img');
+    }
+
+    private function getSharedStimulus(ContainerItemBody $itemBody)
+    {
+        return $itemBody->getComposingElements('oat\taoQtiItem\model\qti\XInclude');
     }
 }

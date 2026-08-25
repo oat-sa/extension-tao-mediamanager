e42c88b5951a16be99186874462381e474681609
KirylHatalski <kirill.gatalsky@taotesting.com>
2026-08-25 16:38:24 +0300

    fix: restrict media Resource Manager filters to audio/video MIME types

--- views/js/qtiCreator/widgets/static/object/states/Active.js
+++ views/js/qtiCreator/widgets/static/object/states/Active.js
@@ -40,10 +40,13 @@ define([
     let $panelObjectSize = null;
     let $panelMediaSize = null;
 
+    const mediaMimeFilters =
+        'video/mp4,video/avi,video/ogv,video/mpeg,video/ogg,video/quicktime,video/webm,video/x-ms-wmv,video/x-flv,audio/mp3,audio/vnd.wav,audio/ogg,audio/vorbis,audio/webm,audio/mpeg,application/ogg,audio/aac,audio/wav,audio/flac';
+
     const _config = {
         renderingThrottle: 1000,
-        fileFilters:
-            'image/jpeg,image/png,image/gif,image/svg+xml,video/mp4,video/avi,video/ogv,video/mpeg,video/ogg,video/quicktime,video/webm,video/x-ms-wmv,video/x-flv,audio/mp3,audio/vnd.wav,audio/ogg,audio/vorbis,audio/webm,audio/mpeg,application/ogg,audio/aac,application/pdf'
+        mediaPlayerMimeType: mediaMimeFilters,
+        fileFilters: mediaMimeFilters
     };
 
     const ObjectStateActive = stateFactory.extend(
@@ -181,6 +184,7 @@ define([
                 pathParam: 'path',
                 path: options.mediaManager.path,
                 root: options.mediaManager.root,
+                currentAsset: $src.val() || undefined,
                 select: function (e, files) {
                     let file, type;
                     if (files && files.length) {

dad4f0d4a1ec2fdf7ab5e1ec735b8dd2f3b287b6
KirylHatalski <kirill.gatalsky@taotesting.com>
2026-09-15 12:17:00 +0300

    fix: point mediaManager searchUrl at scoped search endpoint
    Use config.properties.searchUrl instead of getFilesUrl so asset type filters hit the asset search API.

--- views/js/qtiCreator/helper/creatorRenderer.js
+++ views/js/qtiCreator/helper/creatorRenderer.js
@@ -57,7 +57,7 @@ define([
                     mediaManager: {
                         appendContainer: '#mediaManager',
                         browseUrl: config.properties.getFilesUrl,
-                        searchUrl: config.properties.getFilesUrl,
+                        searchUrl: config.properties.searchUrl,
                         uploadUrl: config.properties.fileUploadUrl,
                         deleteUrl: config.properties.fileDeleteUrl,
                         downloadUrl: config.properties.fileDownloadUrl,
